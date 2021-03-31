c0d2d7bff189de44a9118a8dc42e48a17d67bd48
박도희 <dohee.pa@gmail.com>
2021-03-31 22:56:52 +0900

    77. Sorter Scaffolding

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-console.log('hi there');
+class Sorter {
+  constructor(public collection: number[]) { }
 
-const logSomething = () => {
-  console.log('something');
-};
+  sort(): void {
+
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort()
+console.log(sorter.collection);
