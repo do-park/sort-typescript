8c02d03576e7e317385d211b7a7738399791cd51
박도희 <dohee.pa@gmail.com>
2021-04-03 23:30:16 +0900

    83. Extracting Key Logic

---
+++ build/NumbersCollection.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumberCollection = void 0;
+var NumberCollection = /** @class */ (function () {
+    function NumberCollection() {
+    }
+    return NumberCollection;
+}());
+exports.NumberCollection = NumberCollection;
